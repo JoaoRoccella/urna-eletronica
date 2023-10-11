625f720393eee7ddbdf477917ee7ed5b1aeea174
Joao Roccella <joao.marcos@sp.senai.br>
2023-10-11 12:36:13 +0000

    demo: turma6

---
+++ urnaEletronica.js
@@ -0,0 +1,21 @@
+function urnaEletronica() {
+
+    // Aqui vai todo o código do programa....
+
+    console.log('Iniciando o programa');
+
+    let
+        totalVotosCandidato1 = 0,
+        totalVotosCandidato2 = 0,
+        totalVotosCandidato3 = 0,
+        totalVotosBranco = 0,
+        totalVotosNulo = 0;
+
+    do {
+
+        // instruções
+        
+    } while (condicao);
+
+
+}
