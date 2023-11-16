c737579c6de4641c0f3692365fb8786c349c2b7d
Joao Roccella <joao.marcos@sp.senai.br>
2023-11-16 16:58:25 +0000

    fix: desafio 9 situação de empate

--- urnaEletronica.js
+++ urnaEletronica.js
@@ -1,5 +1,5 @@
 function dataHoraAtual() {
-    
+
     const dataHora = new Date();
     const dia = dataHora.getDate();
     const mes = dataHora.getMonth() + 1;
@@ -13,15 +13,15 @@ function dataHoraAtual() {
 }
 
 async function verificaUrnaAtual() {
-    
+
     let hashUrnaAtual;
     let hashValido;
-        
+
     await fetch('urnaEletronica.js')
         .then(response => response.text())
         .then(response => CryptoJS.SHA256(response).toString())
         .then(response => hashUrnaAtual = response);
-            
+
     await fetch('hashValido')
         .then(response => response.text())
         .then(response => hashValido = response);
@@ -50,8 +50,9 @@ async function audioConfirmacao() {
 
 async function urnaEletronica() {
 
+    // declaração de variáveis
     let candidatos = [
-        [11, 'Gandalf The Grey', 0],
+        [11, 'Gandalf, The Grey', 0],
         [12, 'Frodo Baggins', 0],
         [13, 'Samwise Gamgee', 0],
         [14, 'Pippin Took', 0],
@@ -60,29 +61,35 @@ async function urnaEletronica() {
     let votosBrancos = 0;
     let votosNulos = 0;
     let totalVotos = 0;
-    
+
     let voto;
-    let votoValido = false;
-    
+    let votoValido;
     let ganhador = true;
     let encerrarVotacao;
     let senhaMesario;
-
-    let dataHoraInicial
+    let dataHoraInicial;
     let dataHoraFinal;
 
-    console.log('Início do programa');
-
-    console.log('** CONFIGURAÇÃO DA URNA **');
-    
-    senhaMesario = parseInt(prompt('Defina a senha do mesário:'));
+    console.log(`Início do programa`);
+    dataHoraInicial = dataHoraAtual();
 
     console.clear();
-    
-    // laço de votação
-    dataHoraInicial = dataHoraAtual();
-    
+    console.log(`** CONFIGURAÇÃO DA URNA **`);
+
+    senhaMesario = parseInt(prompt(`Defina a senha de mesário:`));
+
+    document.getElementById('candidatos').innerHTML = 'Opções de voto:<br>';
+    for (let i = 0; i < candidatos.length; i++) {
+        document.getElementById('candidatos').innerHTML += `[${candidatos[i][0]}] ${candidatos[i][1]}<br>`;
+    }
+    document.getElementById('candidatos').innerHTML += `[5] Voto em branco`;
+
+    console.clear();
+
     do {
+
+        votoValido = false;
+
 
         voto = parseInt(prompt('Digite sua opção de voto:'));
 
@@ -90,90 +97,95 @@ async function urnaEletronica() {
 
         for (i = 0; i < candidatos.length; i++) {
             if (voto === candidatos[i][0]) {
-                if (confirm(`
-                Voto selecionado para o candidato ${candidatos[i][1]}. Deseja CONFIRMAR?
-                Pressione [OK] para CONFIRMAR seu voto ou [CANCELAR] para votar novamente.`)) {
-                    console.log(`Votos registrados para o candidato ${candidatos[i][1]} (parcial): ${++candidatos[i][2]}`);
+                if (confirm(`Candidato ${candidatos[i][1]} selecionado. CONFIRMA?`)) {
                     await audioConfirmacao();
+                    console.log(`Votos para o candidato ${candidatos[i][1]} (parcial): ${++candidatos[i][2]}`)
+                } else {
+                    alert(`Voto para o candidato ${candidatos[i][1]} cancelado, VOTE NOVAMENTE.`)
+                    totalVotos--;
                 }
                 votoValido = true;
+                break;
             }
         }
-        
+
         if (!votoValido) {
 
             if (voto === 5) {
-                votosBrancos++;
-                await audioConfirmacao();
-            } else if (voto === senhaMesario) {
-    
-                if (encerrarVotacao = confirm(`
-                Deseja REALMENTE encerrar a votação?
-                Pressione [OK] para ENCERRAR a votação e [CANCELAR] para CONTINUAR a votação.`)) {
+                if (confirm(`Você está VOTANDO EM BRANCO. CONFIRMA?`)) {
+                    await audioConfirmacao();
+                    console.log(`Votos em branco (parcial): ${++votosBrancos}`)
+                } else {
+                    alert(`Voto em branco cancelado, VOTE NOVAMENTE.`)
                     totalVotos--;
                 }
-    
             } else if (voto === 0) {
-                return; // botão de emergência
+                return; 
+            } else if (voto === senhaMesario) {
+
+                // segundo passo de confirmação para encerrar
+                encerrarVotacao = confirm(`Deseja REALMENTE encerrar a votação?`);
+                if (encerrarVotacao) {
+                    totalVotos--;
+                }
+
             } else {
-                if (confirm(`
-                Você votou uma opção inválida (${voto}) e seu voto será ANULADO. 
-                Deseja prosseguir?
-                Pressione [OK] para ANULAR seu voto e [CANCELAR] para CORRIGIR e VOTAR NOVAMENTE.`)) {
-                    votosNulos++;
+
+                if (confirm(`ATENÇÃO: o seu voto será ANULADO. Deseja prosseguir?`)) {
                     await audioConfirmacao();
+                    votosNulos++;
                 } else {
                     totalVotos--;
-                    alert('Voto corrigido, você deve VOTAR NOVAMENTE');
                 }
             }
+
         }
 
     } while (!encerrarVotacao);
-
-    // apresenta os resultados
-    console.clear();
-    console.log('** BOLETIM DE URNA - RESULTADOS **');
-    console.log('Total de votos: ' + totalVotos);
-
-    console.log(`Total de votos do(a) candidato(a) ${nomeCandidato1}: ${votosCandidato1} voto(s) (${(votosCandidato1 / totalVotos * 100).toFixed(2)}%)`);
-
-    console.log(`Total de votos do(a) candidato(a) ${nomeCandidato2}: ${votosCandidato2} voto(s) (${(votosCandidato2 / totalVotos * 100).toFixed(2)}%)`);
-
-    console.log(`Total de votos do(a) candidato(a) ${nomeCandidato3}: ${votosCandidato3} voto(s) (${(votosCandidato3 / totalVotos * 100).toFixed(2)}%)`);
-
-    console.log(`Total de votos brancos: ${votosBrancos} voto(s) (${(votosBrancos / totalVotos * 100).toFixed(2)}%)`);
-
-    console.log(`Total de votos nulos: ${votosNulos} voto(s) (${(votosNulos / totalVotos * 100).toFixed(2)}%)`);
-
-    // determina o ganhador
-    if (votosCandidato1 > votosCandidato2 && votosCandidato1 > votosCandidato3) {
-        nomeGanhador = nomeCandidato1;
-        votosGanhador = votosCandidato1 + votosBrancos;
-    } else if (votosCandidato2 > votosCandidato1 && votosCandidato2 > votosCandidato3) {
-        nomeGanhador = nomeCandidato2;
-        votosGanhador = votosCandidato2 + votosBrancos;
-    } else if (votosCandidato3 > votosCandidato1 && votosCandidato3 > votosCandidato2) {
-        nomeGanhador = nomeCandidato3;
-        votosGanhador = votosCandidato3 + votosBrancos;
-    } else {
-        ganhador = false;
-    }
-
-    // apresenta o ganhador
-    console.log('------');
-
-    if (ganhador) {
-        console.log('O ganhador nesta urna foi o candidato ' + nomeGanhador + ' com ' + votosGanhador + ' voto(s) absoluto(s) (' + (votosGanhador / totalVotos * 100).toFixed(2) + '%)');
-    } else {
-        console.log('Não houve ganhador nesta urna (empate entre dois ou mais candidatos).');
-    }
+    // fim do laço de votação
 
     dataHoraFinal = dataHoraAtual();
-    
-    console.log(`Data/hora de início da votação: ${dataHoraInicial}`);
-    console.log(`Data/hora de encerramento da votação: ${dataHoraFinal}`);
-    
+
+    // Saída para o usuário: boletim de urna
+    console.clear();
+    console.log(`** BOLETIM DE URNA **`);
+    console.log(`Total de votos: ${totalVotos}`);
+
+    // se houver votação
+    if (totalVotos > 0) {
+
+        for (i = 0; i < candidatos.length; i++) {
+
+            console.log(`Total de votos do(a) candidato(a) ${candidatos[i][1]}: ${candidatos[i][2]} votos (${(candidatos[i][2] / totalVotos * 100).toFixed(2)}%)`);
+
+        }
+
+        console.log(`Total de votos brancos: ${votosBrancos} votos (${(votosBrancos / totalVotos * 100).toFixed(2)}%)`);
+
+        console.log(`Total de votos nulos: ${votosNulos} votos (${(votosNulos / totalVotos * 100).toFixed(2)}%)`);
+
+        // determinação do ganhador
+        candidatos.sort((a, b) => b[2] - a[2]);
+
+        if (candidatos[0][2] === candidatos[1][2]) {
+            ganhador = false;
+        }
+
+        // exibição do ganhador
+        console.log(`-------`);
+        if (ganhador) {
+            console.log(`O ganhador desta urna foi ${candidatos[0][1]} com ${candidatos[0][2]} votos (${(candidatos[0][2] / totalVotos * 100).toFixed(2)}%)`);
+        } else {
+            console.log(`Não houve ganhador nesta urna (empate entre 2 ou mais candidatos)`);
+        }
+
+    } else {
+        console.log(`Não houve votação  nesta urna`);
+    }
+
+    console.log(`Data e hora do início da votação: ${dataHoraInicial}`);
+    console.log(`Data e hora do fim da votação: ${dataHoraFinal}`);
+
     await verificaUrnaAtual().then(verificacao => {
         if (verificacao.status) {
             console.log('Hashes verificados, urna íntegra.');
